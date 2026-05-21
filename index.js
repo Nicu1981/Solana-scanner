d3ce3c66293e9de9b67e46da582443472f5d908a
Nicu1981 <nicu02091981@gmail.com>
2026-05-21 14:38:28 +0200

    Add instructions for creating index.js file
    Added instructions for creating and committing index.js.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+Great! Now let's add the main scanner file:
 
+1. Tap the **"+"** button at the top
+2. This time tap **"creating a new file"** again
+3. Name it **index.js**
+4. Paste this code:
+
+1. Tap the **"+"** button → **"Create new file"**
+2. Name it **index.js**
+3. Copy all the code from the file above and paste it
+4. Tap **"Commit changes"**
+
+Come back when done! 👍
